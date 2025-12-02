f13968ea1aec62d8044a09737034b802515d55dd
dsaisnehitha2205@gmail.com <pra.213178882@gmail.com>
2025-12-02 10:13:59 +0530

    First commit

---
+++ src/main/java/com/demo/MavenJava/lab_external/App.java
@@ -0,0 +1,13 @@
+package com.demo.MavenJava.lab_external;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
